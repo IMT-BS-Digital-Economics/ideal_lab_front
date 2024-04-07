2602f88882fe1be4a0e6cc19b59466a18b9554fc
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2024-04-07 02:17:38 +0200

    feat: add new theme to admin panel

--- src/components/admin/displayUsers.js
+++ src/components/admin/displayUsers.js
@@ -1,47 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import {
-    Box,
-    Stack,
-} from "@chakra-ui/react";
+import { Box, Stack } from '@chakra-ui/react';
 
-import hookGetAllUsers from "../../hooks/admin/hookGetAllUsers";
-import UserCard from "./userCard";
+import hookGetAllUsers from '../../hooks/admin/hookGetAllUsers';
+import UserCard from './userCard';
 
 const DisplayUsers = () => {
-    const [usersData, setUsersData] = useState(false);
+  const [usersData, setUsersData] = useState(false);
 
-    hookGetAllUsers({usersData, setUsersData});
+  hookGetAllUsers({ usersData, setUsersData });
 
-    return (
-        <div>
-            <Box m={"3%"} overflowY={"auto"} maxHeight={"48em"} ml={6} mt={6} h={"48em"}>
-                <Stack direction={"column"} spacing={"6"}>
-                    {
-                        usersData ? ( usersData.map((user) => {
-                            return (
-                                <>
-                                {
-                                    user && user.id && user.username ?
-                                        (
-                                            <div key={user.id}>
-                                                <UserCard
-                                                    user={user}
-                                                />
-                                            </div>
-                                        ) : null
-                                }
-                                </>
-                            )
-                        })
-                        ) : (
-                            <></>
-                        )
-                    }
-                </Stack>
-            </Box>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Box
+        m={'3%'}
+        overflowY={'auto'}
+        maxHeight={'48em'}
+        ml={6}
+        mt={6}
+        h={'48em'}
+      >
+        <Stack direction={'column'} spacing={'6'}>
+          {usersData ? (
+            usersData.map((user) => {
+              return (
+                <>
+                  {user && user.id && user.username ? (
+                    <div key={user.id}>
+                      <UserCard user={user} />
+                    </div>
+                  ) : null}
+                </>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </Stack>
+      </Box>
+    </div>
+  );
+};
 
 export default DisplayUsers;
--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,26 +1,19 @@
-import {
-    Grid,
-    GridItem
-} from "@chakra-ui/react";
+import { Grid, GridItem } from '@chakra-ui/react';
 
-import DisplayUsers from "./displayUsers";
-import AddUser from "./addUser";
+import DisplayUsers from './displayUsers';
+import AddUser from './addUser';
 
 const AdminHandler = () => {
-    return (
-        <Grid
-            mt={"3%"}
-            templateRows={'repeat(5, 1fr)'}
-            gap={6}
-        >
-            <GridItem rowSpan={1}>
-                <AddUser/>
-            </GridItem>
-            <GridItem h="100%" rowSpan={4}>
-                <DisplayUsers/>
-            </GridItem>
-        </Grid>
-    );
-}
+  return (
+    <Grid mt={'3%'} templateRows={'repeat(5, 1fr)'} gap={6}>
+      <GridItem rowSpan={1}>
+        <AddUser />
+      </GridItem>
+      <GridItem h="100%" rowSpan={4}>
+        <DisplayUsers />
+      </GridItem>
+    </Grid>
+  );
+};
 
 export default AdminHandler;
