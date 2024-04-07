6ec8417ffe5dadca8abd0a3a86837f31923cce5b
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2024-04-07 18:09:58 +0200

    fix: issue when API Off

--- src/hooks/auth/hookSignIn.js
+++ src/hooks/auth/hookSignIn.js
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
-import { useToast } from "@chakra-ui/react";
+import { useToast } from '@chakra-ui/react';
 
-import axios from "axios";
+import axios from 'axios';
 
-const hookSignIn = ({data, isSubmit, setIsSubmit}) => {
+const hookSignIn = ({ data, isSubmit, setIsSubmit }) => {
     const toast = useToast();
 
     const router = useRouter();
@@ -17,33 +17,40 @@ const hookSignIn = ({data, isSubmit, setIsSubmit}) => {
                 method: 'post',
                 url: `${process.env.NEXT_PUBLIC_HOST}/auth/signin`,
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
+                    'Content-Type': 'application/x-www-form-urlencoded',
                 },
                 withCredentials: true,
-                data: data
-            })
-        }
-        if (isSubmit) {
-            signIn().then((response) => {
-                toast({
-                    title: response.data.detail,
-                    status: "success",
-                    duration: 9000,
-                    isClosable: true
-                });
-                router.push('/')
-            }).catch(error => {
-                toast({
-                    title: "Error",
-                    description: error.response.data.detail,
-                    status: "error",
-                    duration: 9000,
-                    isClosable: true
-                });
-                setIsSubmit(false);
+                data: data,
             });
         }
+        if (isSubmit) {
+            signIn()
+                .then((response) => {
+                    toast({
+                        title: response.data.detail,
+                        status: 'success',
+                        duration: 9000,
+                        isClosable: true,
+                    });
+                    router.push('/');
+                })
+                .catch((error) => {
+                    toast({
+                        title: 'Error',
+                        description:
+                            error.response &&
+                            error.response.data &&
+                            error.response.data.detail
+                                ? error.response.data.detail
+                                : 'Internal Server Error',
+                        status: 'error',
+                        duration: 9000,
+                        isClosable: true,
+                    });
+                    setIsSubmit(false);
+                });
+        }
     });
-}
+};
 
 export default hookSignIn;
